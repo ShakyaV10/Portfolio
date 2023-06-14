3a9626f9e76f124f7d29f577c286fa488bc85499
ShakyaV10 <b420060@iiit-bh.ac.in>
2023-06-14 22:21:52 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Experience from "./components/Experience";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Porfolio from "./components/Porfolio";
+import SocialLinks from "./components/SocialLinks";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Home />
+      <About />
+      <Porfolio />
+      <Experience />
+      <Contact />
+
+      <SocialLinks/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const About = () => {
+  return (
+  <div name="about" className='w-full h-fit md:h-screen bg-gradient-to-b from-gray-800 
+  to-black text-white'>
+
+    <div className='max-w-screen-lg p-4 mx-auto flex flex-col
+        justify-center w-full h-full'>
+        <div className='pb-8 pt-20'>
+            <p className='text-4xl font-bold inline border-b-4
+            border-gray-500'>
+                About
+            </p>
+        </div>
+
+        <p className='text-xl mt-20'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi explicabo labore, veniam dignissimos consectetur veritatis, possimus a sunt libero quis, magni adipisci officiis cumque tempore suscipit soluta voluptatum omnis ut natus? Adipisci explicabo beatae ad error perferendis eaque harum aperiam, quam quas. Est laboriosam mollitia ut voluptatibus perspiciatis eaque ducimus?
+        </p>
+
+        <br />
+        
+        <p className='text-xl'>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda adipisci nesciunt, inventore obcaecati laborum natus modi vitae odit delectus rem libero eum placeat! Omnis debitis dolores fugiat eveniet reiciendis? Est facere, impedit laborum numquam consectetur repellendus pariatur eligendi doloribus sit et consequuntur asperiores tempore repellat fugit, voluptates dolores velit blanditiis!
+        </p>
+
+    </div>
+
+  </div>
+)
+}
+
+export default About
